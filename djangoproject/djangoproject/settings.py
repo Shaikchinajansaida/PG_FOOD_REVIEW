d6bdfd5b05a09c942a010ec621f403056bfe7dc3
anudeepgadari07 <anudeepgadari@gmail.com>
2026-01-29 13:08:45 +0530

    added pem file

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -83,9 +83,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        OPTIONS: {
+        'OPTIONS': {
             'ssl': {
-                'ca': None,
+                'ca': os.getenv('pem_content'),
             },
         },
     },
